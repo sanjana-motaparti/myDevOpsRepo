be16518a321f6e103cdeb4590fd5300f438d78ea
UIT2022842 <sanjana-motaparti>
2026-02-10 10:24:57 +0530

    Add calculator class

---
+++ calculator.java
@@ -0,0 +1,8 @@
+public class calculator{
+    public int addNumbers(int x, int y){
+        int sum=0;
+        sum=x+y;
+
+        return sum;
+    }
+}
---
+++ calculatorTest.java
@@ -0,0 +1,17 @@
+import java.beans.Transient;
+
+import org.junit.*;
+
+public class calculatorTest {
+
+    @Test
+
+    public void test1forCalc(){
+        calculator c=new calculator();
+
+        Assert.assertEquals(Math.addExact(4, 3),c.addNumbers(4, 3));
+        Assert.assertEquals(Math.addExact(4, -4),c.addNumbers(4, -4));
+        Assert.assertNotEquals(0,c.addNumbers(1, 2));
+    }
+    
+}
